53233ec3d1e4eca46aa80cb93cb2b2a40c5b5cbe
renoaldo <renoaldo@hotmail.com>
2024-10-16 17:41:15 -0300

    Faz teste na API, Faz analise nos dados e Plota Gráfico com plotly.

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def load_css():
     """Função para injetar CSS para ocultar elementos e definir estilos."""
     css = """
         <style>
-        #MainMenu {visibility: hidden;} /* Ocultar o menu principal do Streamlit */
-        header {visibility: hidden;} /* Ocultar o cabeçalho inteiro */
+        /*#MainMenu {visibility: hidden;} /* Ocultar o menu principal do Streamlit */
+        /*header {visibility: hidden;} /* Ocultar o cabeçalho inteiro */
         .st-emotion-cache-12fmjuu {padding-top: 0rem; margin-top: 0rem;} /* Reduz o padding e margin para o cabeçalho */
         .css-18e3th9 {padding-top: 0rem; padding-bottom: 0rem; background-color: white;} /* Ajusta o padding e define fundo da área principal */
         .css-1d391kg {background-color: white;} /* Define a cor de fundo da sidebar */
